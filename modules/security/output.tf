d946ead4867b1bd4a1ea93763bb54e49f7d37ac1
laulv <levanlau.net@gmail.com>
2025-06-15 14:33:32 +0700

    update

--- modules/security/output.tf
+++ modules/security/output.tf
@@ -2,24 +2,3 @@
 output "bastion_sg_id" {
   value = aws_security_group.bastion_sg.id
 }
-
-output "alb_sg_id" {
-  value = aws_security_group.alb_sg.id
-}
-
-output "app_sg_id" {
-  value = aws_security_group.app_sg.id
-}
-
-output "postgresql_sg_id" {
-  value = aws_security_group.postgresql_sg
-}
-
-# ===============================
-
-output "public_sg_id" {
-  value = aws_security_group.public_sg.id
-}
-output "private_sg_id" {
-  value = aws_security_group.private_sg.id
-}
